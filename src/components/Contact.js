9350a20f300ce3230878fd12480ae15190d4e8f9
rexhang0501 <Rex.Hang0501@gmail.com>
2023-08-03 05:11:10 +0800

    Modify contact page css

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,7 @@
 const Contact = () => {
     return(
         <div name="contact" className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4">
-            <form action="" className="flex flex-col max-w-[600px] w-full">
+            <form action="" className="flex flex-col max-w-[1000px] w-full">
                 <div className="pb-8">
                     <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">Contact</p>
                     <p className="text-gray-300 py-4">// Submit the form below of shoot me and email </p>
@@ -9,7 +9,7 @@ const Contact = () => {
                 <input type="text" placeholder="Name" name='name' className="p-2 bg-[#ccd6f6]"/>
                 <input type="email" placeholder="Email" name='email' className="my-4 p-2 bg-[#ccd6f6]"/>
                 <textarea name="message" rows="10" placeholder="Message" className="p-2 bg-[#ccd6f6]"></textarea>
-                <button className="text-gray-300 border-2 hover:bg-pink-600 px-4 py-3 my-8 items-center duration-500">Nigg</button>
+                <button className="text-gray-300 border-2 hover:bg-pink-600 px-4 py-3 my-8 items-center duration-500">Submit !</button>
             </form>
         </div>
     );
